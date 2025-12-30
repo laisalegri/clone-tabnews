3df86c56a6293d3192b1f8f18928b23485add378
Laís Alegri <leislaa@gmail.com>
2025-12-30 20:48:33 +0000

    modifica texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Oi mor, to fazendo a primeiro deploy do meu curso, e queria dizer que eu amo vc ♥! :) </h1>
+    return <h1>Oi mor, to fazendo a primeiro deploy do meu curso, e queria dizer que eu amo vc! :) ♥ </h1>
 }
 
 export default Home;
