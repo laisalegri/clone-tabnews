961474163990c4d3504555e29a4a754e2f5c6fbc
Laís Alegri <leislaa@gmail.com>
2026-01-04 15:10:38 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-function Home(){
-    return <h1>Oi mor, to fazendo o primeiro deploy do meu curso, e queria dizer que eu amo vc! :) ♥ </h1>
+function Home() {
+  return (
+    <h1>
+      Oi mor, to fazendo o primeiro deploy do meu curso, e queria dizer que eu
+      amo vc! :) ♥{" "}
+    </h1>
+  );
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
