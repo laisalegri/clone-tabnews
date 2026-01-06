5244c3d8ea19cbabbd7300164991ea66c978ce46
Laís Alegri <leislaa@gmail.com>
2026-01-06 23:18:51 +0000

    adiciona `prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,9 @@ function Home() {
   return (
     <h1>
       Oi mor, to fazendo o primeiro deploy do meu curso, e queria dizer que eu
-      amo vc! :) ♥{" "}
+      amo vc! :) ♥
     </h1>
   );
-}
-
-function teste2() {
-  console.log("indentação errada");
 }
 
 export default Home;
