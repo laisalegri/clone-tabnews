f2223803c44e9c9b6eebaf2398911064fcb3be09
Laís Alegri <leislaa@gmail.com>
2026-01-09 00:23:31 +0000

    alterando msg

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 function Home() {
-  return (
-    <div style="margin: 40% auto">
-      <img src="https://i.ibb.co/DD1dVH5w/logo.png" alt="logo" border="0"></img>
-    </div>
-  );
+  return <h1>Rodabeda feelings</h1>;
 }
 
 export default Home;
