6b3ca3679a783e14b75f810c3957596d6dca5018
Laís Alegri <leislaa@gmail.com>
2026-01-13 22:53:08 +0000

    Rodabeda página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
+import { useEffect } from "react";
+
 function Home() {
+  useEffect(() => {
+    document.body.style.margin = "0";
+    document.body.style.backgroundColor = "#410041";
+  }, []);
+
   return (
     <div
       style={{
-        margin: 0,
-        backgroundColor: "purple",
         minHeight: "100vh",
-
         display: "flex",
+        flexDirection: "column", // coloca um em cima do outro
         justifyContent: "center",
         alignItems: "center",
+        textAlign: "center", // centraliza texto
+        color: "purple", // cor do texto (opcional)
       }}
     >
+      <h1 style={{ marginBottom: "20px" }}>Rodabeda ♥</h1>
+
       <img
         src="https://i.ibb.co/G4y8r2f2/logo.png"
         alt="Rodabeda"
