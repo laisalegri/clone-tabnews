bbccef058efa47e5e62f6b030c1dffeda9e6732d
Laís Alegri <leislaa@gmail.com>
2026-01-13 23:03:58 +0000

    Rodabeda fonte nova

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,21 @@ import { useEffect } from "react";
 
 function Home() {
   useEffect(() => {
+    // fundo do body
     document.body.style.margin = "0";
     document.body.style.backgroundColor = "#410041";
+
+    // adiciona fonte no <head>
+    const link = document.createElement("link");
+    link.href =
+      "https://fonts.googleapis.com/css2?family=Chocolate+Classical+Sans&display=swap";
+    link.rel = "stylesheet";
+    document.head.appendChild(link);
+
+    // limpeza opcional
+    return () => {
+      document.head.removeChild(link);
+    };
   }, []);
 
   return (
@@ -13,14 +26,21 @@ function Home() {
       style={{
         minHeight: "100vh",
         display: "flex",
-        flexDirection: "column", // coloca um em cima do outro
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        textAlign: "center", // centraliza texto
-        color: "purple", // cor do texto (opcional)
+        textAlign: "center",
+        color: "white",
       }}
     >
-      <h1 style={{ marginBottom: "20px" }}>Rodabeda ♥</h1>
+      <h1
+        style={{
+          marginBottom: "20px",
+          fontFamily: '"Chocolate Classical Sans", sans-serif',
+        }}
+      >
+        Rodabeda ♥
+      </h1>
 
       <img
         src="https://i.ibb.co/G4y8r2f2/logo.png"
