fd61a125fd033de9413afcec5e50762c7fcd558e
Laís Alegri <leislaa@gmail.com>
2026-01-13 22:40:24 +0000

    Rodabeda logo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
 function Home() {
-  return <h1>Rodabeda ♥</h1>;
+  return (
+    <div
+      style={{
+        margin: 0,
+        backgroundColor: "purple",
+        minHeight: "100vh",
+
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <img
+        src="https://i.ibb.co/G4y8r2f2/logo.png"
+        alt="Rodabeda"
+        style={{
+          width: "100%",
+          maxWidth: "500px",
+          height: "auto",
+        }}
+      />
+    </div>
+  );
 }
 
 export default Home;
